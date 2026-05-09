680beee9323f3e163288570b6598a425c81e0e27
JawlessMamba <mh.ansari1000@gmail.com>
2026-05-10 01:52:24 +0500

    Update website content and script

--- HappyBirthdayGF/script.js
+++ HappyBirthdayGF/script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "Mom You're the most adorable a gem of a person! 💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
